1afc949a61352d38cee52b33494d054321d77d4f
Roshan Poudel <1.rosanpoudel@gmail.com>
2026-04-26 12:45:56 +0545

    chore : implicitly add auth secret

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -4,6 +4,7 @@ import { NextAuthOptions } from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 
 export const authOptions: NextAuthOptions = {
+  secret: process.env.NEXTAUTH_SECRET,
   adapter: PrismaAdapter(prisma),
   providers: [
     GoogleProvider({
